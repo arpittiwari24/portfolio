b43b71e3f7afa8ad9c6a589caaa80e2f96340634
arpittiwari24 <100207940+arpittiwari24@users.noreply.github.com>
2023-08-10 13:55:20 +0530

    asdfgh

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
     <div className='px-6 '>
       <h1 className="text-white max-sm:text-5xl text-7xl font-bold ">Hello !!</h1>
       <p className="text-white text-3xl py-10 ">I am Arrpit Tiwari , a Full-Stack Developer who codes like a wizard and debugs like a pro, turning bugs into distant memories, and websites into techno masterpieces! </p>
-      <a href="https://bit.ly/3Xo9uay" target="_blank"><button  className="btn btn-warning animate-bounce hover:btn-success">View  Resume</button></a>
+      <a href="https://bit.ly/3YwH7I4" target="_blank"><button  className="btn btn-warning animate-bounce hover:btn-success">View  Resume</button></a>
     </div>
   </div>
 </div>
